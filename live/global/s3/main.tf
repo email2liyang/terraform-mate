826b2ad7baa741a032870d205967b2d27b5cc98c
ivan <ivan.li@practiceinsight.io>
2018-09-07 16:37:52 +0800

    add rw user

--- live/global/s3/main.tf
+++ live/global/s3/main.tf
@@ -20,6 +20,40 @@ resource "aws_s3_bucket" "corp_artifact_repo" {
   }
 
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
+}
+
+# we need a service account user
+resource "aws_iam_user" "s3_repo_user_rw" {
+  name = "srv_${var.bucket_name}_rw"
+}
+
+# generate keys for service account user
+resource "aws_iam_access_key" "s3_repo_user_keys_rw" {
+  user = "${aws_iam_user.s3_repo_user_rw.name}"
+}
+
+# grant user access to the bucket
+resource "aws_s3_bucket_policy" "bucket_policy_rw" {
+  bucket = "${aws_s3_bucket.corp_artifact_repo.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_iam_user.s3_repo_user_rw.arn}"
+      },
+      "Action": [ "s3:*" ],
+      "Resource": [
+        "${aws_s3_bucket.corp_artifact_repo.arn}",
+        "${aws_s3_bucket.corp_artifact_repo.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
 }
--- live/global/s3/outputs.tf
+++ live/global/s3/outputs.tf
@@ -1,3 +1,28 @@
 output "s3_bucket_arn" {
   value = "${aws_s3_bucket.corp_artifact_repo.arn}"
 }
+
+//the arn of the user that was created
+output "user_arn_rw" {
+  value = "${aws_iam_user.s3_repo_user_rw.arn}"
+}
+
+//the name of the service account user that was created
+output "user_name_rw" {
+  value = "${aws_iam_user.s3_repo_user_rw.name}"
+}
+
+//the name of the bucket
+output "bucket_name" {
+  value = "${aws_s3_bucket.corp_artifact_repo.id}"
+}
+
+//the access key
+output "iam_access_key_id_rw" {
+  value = "${aws_iam_access_key.s3_repo_user_keys_rw.id}"
+}
+
+//the access key secret
+output "iam_access_key_secret_rw" {
+  value = "${aws_iam_access_key.s3_repo_user_keys_rw.secret}"
+}
