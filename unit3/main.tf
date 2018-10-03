06d8947d5216ca956d834ba2a9825620b9f8b5bb
ivan <ivan.li@practiceinsight.io>
2018-10-03 16:00:37 +0800

    add output

--- unit3/main.tf
+++ unit3/main.tf
@@ -2,3 +2,23 @@ provider "aws" {
   region  = "${var.region}"
   profile = "${var.profile}"
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+
+  enable_nat_gateway     = false
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
