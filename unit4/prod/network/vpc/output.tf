f7320d102b72c7bfc61f852a184581e3563ddbd7
ivan <ivan.li@practiceinsight.io>
2018-10-13 14:48:21 +0800

    vpc_default_security_group_id

--- unit4/prod/network/vpc/output.tf
+++ unit4/prod/network/vpc/output.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = "${module.vpc.vpc_id}"
 }
 
+output "vpc_default_security_group_id" {
+  value = "${module.vpc.default_security_group_id}"
+}
+
 output "vpc_public_subnets" {
   value = "${module.vpc.public_subnets}"
 }
