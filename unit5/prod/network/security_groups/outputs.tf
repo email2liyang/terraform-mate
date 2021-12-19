eb8fbb27d529fe7638a1696e7aa142d4c6c827f3
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-19 08:36:06 +0800

    Delete unit5 directory

--- unit5/prod/network/security_groups/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ssh_security_group_id" {
-  value = "${module.ssh_security_group.this_security_group_id}"
-}
--- unit5/prod/network/vpc/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  default = "ap-southeast-2"
-}
-
-variable "profile" {
-  description = "aws profile to use"
-  default     = "psn"
-}
